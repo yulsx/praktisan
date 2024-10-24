d67c77a8465f7f29d2f6d2af054ced47830755e3
yulsx <punzalan.eulhanmhar.lucena@gmail.com>
2024-10-24 17:37:20 +0800

    first half commit

--- first.php
+++ first.php
@@ -1,5 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+</head>
+
+<body>
+    <h1>PHP</h1>
+
+    <?php
+
 
 
 ?>
+
+</body>
+
+</html>
+
+
+
+
 
